a0a9387f025a42a0bd1078e6c44158b0657e9193
Gaston Bengolea <gastonbengolea@gmail.com>
2014-09-02 06:16:20 -0300

    refactoring

--- finger.py
+++ finger.py
@@ -2,9 +2,6 @@ from vector import vector
 from segment import segment
 import math, pygame
 
-def vector_angle(angle, length):
-    return vector(math.cos(angle), -math.sin(angle)) * length
-
 class finger:
 
     def __init__(self, pivot, length, min_angle, max_angle, color):
@@ -29,10 +26,19 @@ class finger:
 
     def trigger(self):
         self.triggered = True
+
+    def segments(self):
+        return segment(self.pivot, self.extreme(), self.color)
     
     def segment(self):
         return segment(self.pivot, self.extreme(), self.color)
-    
+
+    def extreme(self):
+        return self.pivot + self.angular_vector(self.angle, self.length)
+
+    def ang1(self):
+        return self.angle + math.pi / 2
+
     def draw(self, screen):
         self.segment().draw(screen)
         pygame.draw.circle(screen, self.color, (self.pivot.x, self.pivot.y), 8)
@@ -40,26 +46,20 @@ class finger:
         pygame.draw.circle(screen, self.color, (extreme.x, extreme.y), 5)
         angle1 = self.ang1()
         angle2 = angle1 + math.pi
-        ext11 = self.pivot + vector(math.cos(angle1), math.sin(angle1)) * 8
-        ext12 = self.pivot + vector(math.cos(angle2), math.sin(angle2)) * 8
-        ext21 = extreme + vector(math.cos(angle1), math.sin(angle1)) * 5
-        ext22 = extreme + vector(math.cos(angle2), math.sin(angle2)) * 5
-        pygame.draw.line(screen, self.color, ext11.to_tuple(), ext21.to_tuple())
+        ext11 = self.pivot + self.angular_vector(angle1, 8)
+        ext12 = self.pivot + self.angular_vector(angle2, 8)
+        ext21 = extreme + self.angular_vector(angle1, 5)
+        ext22 = extreme + self.angular_vector(angle2, 5)
+        pygame.draw.line(screen, (255,0,255), ext11.to_tuple(), ext21.to_tuple())
         pygame.draw.line(screen, self.color, ext12.to_tuple(), ext22.to_tuple())
 
 
 class left_finger(finger):
 
-    def extreme(self):
-        return self.pivot + vector_angle(self.angle, self.length)
-
-    def ang1(self):
-        return -self.angle + math.pi / 2
+    def angular_vector(self, angle, length):
+        return vector(math.cos(angle), -math.sin(angle)) * length
 
 class right_finger(finger):
 
-    def extreme(self):
-        return self.pivot + vector_angle(self.angle, self.length).horizontal_mirror()
-
-    def ang1(self):
-        return self.angle + math.pi / 2
+    def angular_vector(self, angle, length):
+        return (vector(math.cos(angle), -math.sin(angle)) * length).horizontal_mirror()
